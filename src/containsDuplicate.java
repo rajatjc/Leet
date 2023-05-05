01b2cd6ed403917a3c7f86abe36054c2a9a2e616
rajatjc <rajat.sharma4211@gmail.com>
2023-05-05 17:08:19 -0400

    217 done

--- src/containsDuplicate.java
+++ src/containsDuplicate.java
@@ -1,9 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class containsDuplicate {
+
     public boolean containsDuplicate(int[] nums) {
 
+        Map<Integer,Integer> h=new HashMap<>();
+        for (int i : nums) {
+            if(h.containsKey(i))
+                return true;
+            else
+                h.put(i,1);
 
-
-
+        }
         return false;
+    }
+
+    public void main(String[] args) {
+        boolean ans = containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2});
+        System.out.println(ans);
     }
 }
