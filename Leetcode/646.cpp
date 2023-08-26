f6818b118b67f9c77f8e40f204ec1312ca4116fd
Rajat Sharma <32574865+rajatjc@users.noreply.github.com>
2023-08-25 22:03:36 -0400

    646  added

---
+++ Leetcode/646.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        // Sort the pairs based on the second element (righti)
+        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        
+        int n = pairs.size();
+        vector<int> dp(n, 1); // Initialize dp array
+        
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        return *max_element(dp.begin(), dp.end()); // Return the maximum value in dp
+    }
+};
